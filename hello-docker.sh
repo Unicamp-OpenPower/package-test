8d31d458984f6bec377101fdf1434ec7382f41db
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-11-03 17:20:09 -0300

    test7

--- hello-docker.sh
+++ hello-docker.sh
@@ -1,5 +1,5 @@
 printf "deb https://oplab9.parqtec.unicamp.br/pub/repository/debian/ ./" >> /etc/apt/sources.list
 wget https://oplab9.parqtec.unicamp.br/pub/key/openpower-gpgkey-public.asc
-apt-key add openpower-gpgkey-public.asc
-apt-get update
+sudo apt-key add openpower-gpgkey-public.asc
+sudo apt-get update
 echo "Hello world"
