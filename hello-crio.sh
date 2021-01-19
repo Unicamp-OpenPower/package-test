f523238e116c8c53f008ce08c9ba58f641fea01d
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2021-01-19 17:13:43 -0300

    crete tes for crio

--- hello-crio.sh
+++ hello-crio.sh
@@ -2,7 +2,7 @@ printf "deb https://oplab9.parqtec.unicamp.br/pub/repository/debian/ ./" >> /etc
 wget https://oplab9.parqtec.unicamp.br/pub/key/openpower-gpgkey-public.asc
 sudo apt-key add openpower-gpgkey-public.asc
 sudo apt-get update
-sudo apt install -y git make crio golang-go
+sudo apt install -y git make crio
 sudo systemctl daemon-reload
 sudo systemctl enable crio
 sudo systemctl start crio
@@ -11,7 +11,6 @@ sudo cp go/bin/crictl /usr/local/bin/
 sudo crictl --runtime-endpoint unix:///var/run/crio/crio.sock version
 git clone https://github.com/cri-o/cri-o.git
 cd cri-o/
-sudo mkdir /etc/containers/
 sudo cp test/policy.json /etc/containers
 POD_ID=$(sudo crictl runp test/testdata/sandbox_config.json)
 sudo crictl inspectp --output table $POD_ID
